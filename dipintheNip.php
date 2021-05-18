73d9dbd6a3de807165240ee03fa6f473f9e90755
Jeffy <abraham_jeffy@yahoo.com>
2021-05-19 00:12:43 +0530

    Update dipintheNip.php

--- dipintheNip.php
+++ dipintheNip.php
@@ -17,7 +17,7 @@
                         <img class="img-fluid" src="./img/Dip-in-Nip-A4-2020.jpg" alt="Dip In the Nip">
                     </div>
                     
-                    <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3">
+                    <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
                         <div class="embed-responsive embed-responsive-16by9">
                             <video  class="embed-responsive-item" controls>
                                 <source src="./vid/dipNnip.mp4" type="video/mp4">
@@ -26,23 +26,10 @@
                     </div>
 
                     <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
-                        <h4>STEP 1: HOW TO REGISTER FOR OUR 10TH DIP IN THE NIP</h4>
+                        <h4>HOW TO REGISTER FOR OUR 10TH DIP IN THE NIP</h4>
                         <p>Simply fill in the form below and we will post a Registration Form and Sponsorship Card to you within a few days. Please complete this form and send back to us by post or email.</p>
                         <p>If you are intending to organize a Group to Dip together and have 5 or more ladies, please email <a href="mailto:necret.info@gmail.com">necret.info@gmail.com</a> or text <a href="tel:00353867773294">086 7773294</a> with your name, address and the names in your group. We also ask that your group has a name – we had some brilliant names over the years.</p>
                         <p>We have a different coloured registration form for groups which we can group post out to you along with the individual sponsorship cards or send to each group member.</p>
-                    </div>
-                    <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
-                        <h4>STEP 2: MEET THE OTHER DIPPERS!</h4>
-                        <p>Come along at 9.00pm to our meet up in the Westcourt Hotel Drogheda the night before the Dip. Get to know your fellow Dippers. Lots of fun with drinks and finger food and there might even be a bit of time for some real body painting so as to get you ready for the big day.</p>
-                    </div>
-                    <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
-                        <h4>STEP 3: READY… STEADY… DIP!</h4>
-                        <p>Gather at 7.30am at the Westcourt Hotel, Drogheda. Submit your sponsorship money to the fundraising team and then get limbered up for your Dip with a fun-filled Zumba session and the customary complimentary Dutch Courage reception.</p>
-                        <p>Climb aboard a nice warm coach to bring you to the beach (most definitely a secret location). Arrive at the beach for dipping at 9.15am. Before you dip you’ll take time at the now famous “Wishing Tree” where you can write and leave messages for your loved ones. Then after the actual dip enjoy a well-earned hot breakfast served on the beach by our “Surprise Guests”, pose for a few photos while taking in a few laughs with other like-minded dippers. Collect your certificate of achievement, be part of the prize giving ceremony for the best-dressed non-dressed person all before climbing back on board the coach to return to the Westcourt Hotel.</p>
-                    </div>
-                    <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
-                        <h4>STEP 4: FEEL EMPOWERED!…</h4>
-                        <p>Return with a warm glow and a proud feeling while having a cup of tea and a bun served between the Hotel and McCloskey’s Bakery. Feel the satisfaction of knowing that your Dip will help people right across the region under-going cancer treatment.</p>
                     </div>
                     <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
                         <h4>BY THE WAY… WHAT IS NECRET?</h4>
@@ -57,33 +44,14 @@
                 </div>
             </div>
         </div>
-
         <?php include 'includes/contactForm.php'; ?>
-
         <div id="cuttingEdge" class="container-fluid">
             <div class="container">
                 <div class="row pt-2">
                     <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
                         <h4>TODAY:</h4>
-                        <p>After you fill in the contact box above we will then post out your registration form and sponsorship card in the next few days.</p>
+                        <p>After you fill in the contact box above we will then post out your registration form and sponsorship card in the next week or so.</p>
                     </div>
-                    <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
-                        <h4>06:45 AM:</h4>
-                        <p>Everyone arrives at the Westcourt Hotel in Drogheda. Sign in takes place and sponsorship money is handed over, some ladies get dressed up as in body paint, ware wigs, and long necklaces, then we do a bit of Zumba dancing to warm up.</p>
-                    </div>
-                    <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
-                        <h4>08:00 AM – 08:15 AM:</h4>
-                        <p>Buses are provided to bring you to the Beach, we arrive there about 8.30 am a bit more dancing</p>
-                    </div>
-                    <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
-                        <h4>09:00 AM:</h4>
-                        <p>This is the approximate time for when the Dip takes place after the dip you will be having a laugh and a warm cup of tea and hot dog. We then give out a few fun best non dressed prizes to both groups and individuals.</p>
-                    </div>
-                    <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3 text-center">
-                        <h4>11.00 AM:</h4>
-                        <p>We head back to the Westcourt Hotel to change clothes and then across the road to the famous Moorland Cafe (Mc Closkey’s) on West Street for a well earned coffee and a bun!!</p>
-                    </div>
-
                     <div class="col-md-12 col-12 col-lg-12 col-xl-12 col-sm-12 mt-3 pt-2 mb-3">
                         <div class="embed-responsive embed-responsive-16by9">
                             <video  class="embed-responsive-item" controls>
