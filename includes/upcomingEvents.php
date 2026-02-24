a04eb5c51a23a771f6ecd1ceaf09fb9c509d0709
Pierce Jordan <pjordan@irelandxo.com>
2026-02-24 14:19:18 +0000

    PJ - Upcoming

--- includes/upcomingEvents.php
+++ includes/upcomingEvents.php
@@ -4,28 +4,6 @@
         <br>
         <div class="div col col-sm-12 col-12 col-md-5 col-lg-3 col-xl-3 d-flex">
             <div class="card navyCard flex-fill">
-                <iframe class="mx-auto" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F3214477722026650%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
-                <div class="card-body navyCardBody d-flex flex-column">
-                    <h5 class="card-title navyCardTitle">The Ultimate Golf Giveaway</h5>
-                    <p class="card-text navyCardText">We are very, VERY excited to announce our brand new fundraiser - <strong>The Ultimate Golf Giveaway</strong></p>
-                    <p class="card-text navyCardText"><strong><em>Eight great prizes... one great cause</em></strong></p>
-                    <p class="card-text navyCardText">Enjoy the chance to win incredible golf play-and-stay experiences across some of Ireland’s finest resorts, including 
-                        Doonbeg International Golf Links, The K Club, Druids Glen and more. Each prize includes luxurious accommodation, fine dining and a round of golf at 
-                        world-class courses.</p>
-                    <p class="card-text navyCardText"><strong>Buy your tickets here -> <a href="https://necretfundraisers.ie" target="_blank">https://necretfundraisers.ie</a></strong>  </p>
-                    <p class="card-text navyCardText">Every ticket sold helps support NECRET’s vital work in cancer research and care here in the North East.</p>
-                    <h5>Tickets</h5>
-                    <p class="card-text navyCardText">1 Ticket - €20</p>
-                    <p class="card-text navyCardText">3 Tickets - €50</p>
-                    <p class="card-text navyCardText">7 Tickets - €100</p>
-                    <p class="card-text navyCardText">Make your next round count — for yourself and for a cause that changes lives.</p>
-                    <p class="card-text navyCardText"><a href="https://www.facebook.com/hashtag/necretfightingcancereveryday" target="_blank">#NECRETfightingcancereveryday</a></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="div col col-sm-12 col-12 col-md-5 col-lg-3 col-xl-3 d-flex">
-            <div class="card navyCard flex-fill">
                 <img class="card-img-top" src="./img/CaminoBoyne_2026.jpg" alt="Message of Hope">
                 <div class="card-body navyCardBody d-flex flex-column">
                     <h5 class="card-title navyCardTitle">Boyne Valley Camino</h5>
@@ -35,6 +13,22 @@
                     <p class="card-text navyCardText">To receive your registration pack please email <a href="mailto:necret.info@gmail.com">necret.info@gmail.com</a> 
                     or text <a href="tel:+353867773294"></a> 086 7773294 with your full name, address, and mobile number. To participate completed registration forms 
                     must be received no later than midday on Friday July 13th.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="div col col-sm-12 col-12 col-md-5 col-lg-3 col-xl-3 d-flex">
+            <div class="card navyCard flex-fill">
+                <img class="card-img-top" src="./img/dipNTheNip2026.jpg" alt="DipNip2026">
+                <div class="card-body navyCardBody d-flex flex-column">
+                    <h5 class="card-title navyCardTitle">Dip in The Nip</h5>
+                    <h5 class="card-title navyCardTitle">WILL YOU BE BRAVE ENOUGH TO DIP?</h5>
+                    <p class="card-text navyCardText">The 15th <em>Northeast Dip in the Nip</em> – 13th September 2026</p>
+                    <p class="card-text navyCardText">This is not just a swim.</p>
+                    <p class="card-text navyCardText">It’s a moment of madness for a great cause. A morning of magic for your self-belief.</p>
+                    <p class="card-text navyCardText">And a memory that will stay with you forever.</p>
+                    <p class="card-text navyCardText">We’re calling on <u>women over the age of 18</u> to take part in the 15th annual Dip in the Nip – 
+                        a powerful and emotional fundraiser supporting NECRET, helping cancer patients right here in Our Lady of Lourdes Oncology unit in the North East.</p>
                 </div>
             </div>
         </div>
